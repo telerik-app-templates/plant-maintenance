7690391131250155a94ddcaaa254bef3032dcb25
Enterprise Solutions <telerik.enterprise.solutions@gmail.com>
2015-05-17 18:08:16 +0000

    Remove unused function guid.

--- Offline Data Sync/scripts/app/appstorage.js
+++ Offline Data Sync/scripts/app/appstorage.js
@@ -5,18 +5,6 @@ app.AppStorage = (function () {
     
     var currentUserId;
     
-    var guid = (function() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-        }
-        return function() {
-            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-                s4() + '-' + s4() + s4() + s4();
-        };
-    })();
-    
 	var online = (window.localStorage.getItem("online") === 'true');
     
     // locations
